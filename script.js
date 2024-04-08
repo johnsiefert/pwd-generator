ac984f053a20f75903da1d2f037d59d00a1724dc
John <johnsief12@gmail.com>
2024-04-08 18:04:55 -0400

    dynamically add two passwords

---
+++ script.js
@@ -0,0 +1,118 @@
+const btn = document.querySelector('#btn');
+let input1 = document.querySelector('.pwd-input');
+let input2 = document.querySelector('.pwd-input-1');
+
+const characters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '~',
+  '`',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '_',
+  '-',
+  '+',
+  '=',
+  '{',
+  '[',
+  '}',
+  ']',
+  ',',
+  '|',
+  ':',
+  ';',
+  '<',
+  '>',
+  '.',
+  '?',
+  '/',
+];
+
+
+function getRandomPassword() {
+let password = characters.join('');
+let result = "";
+  for(let i = 0; i < 15; i++){
+    let randomIndex = Math.floor(Math.random() * password.length);
+    result += password.charAt(randomIndex)
+  }
+return result
+}
+
+function getPassword() {
+  let password1 = getRandomPassword();
+  let password2 = getRandomPassword();
+input1.value = password1
+input2.value = password2;
+}
+
+
+btn.addEventListener('click', getPassword)
